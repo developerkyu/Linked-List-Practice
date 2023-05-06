3c21ff6562e956286709dd1219a2916a11d1b7af
developerkyu <dlrbwls0302@gmail.com>
2023-05-06 16:10:57 +0900

    Initial Commit

---
+++ Linked-List-Practice/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Linked-List-Practice
+//
+//  Created by 이규진 on 2023/05/06.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
