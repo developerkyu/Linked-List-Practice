f0a4bd0b4647766724558e3f08e137597cf35662
developerkyu <dlrbwls0302@gmail.com>
2023-05-13 10:20:35 +0900

    add: 세 번째 노드 뒤에 새로운 노드 삽입하는 코드 추가

--- Linked-List-Practice/Test_LinkedList.c
+++ Linked-List-Practice/Test_LinkedList.c
@@ -35,6 +35,23 @@ int main(){
         printf("List[%d]: %d \n", i, Current -> Data);
     }
     
+    // 두 번째 노드 뒤에 3000 삽입
+    printf("\nInserting 3000 After [2]...\n\n");
+    
+    Current = SLL_GetNodeAt(List, 2);
+    NewNode = SLL_CreateNode(3000);
+    
+    SLL_InsertAfter(Current, NewNode);
+    
+    count = SLL_GetNodeCount(List);
+    
+    // 링크드 리스트 출력
+    for(int i = 0; i < count; i++){
+        Current = SLL_GetNodeAt(List, i);
+        printf("List[%d]: %d \n", i, Current -> Data);
+    }
+    
+    
     // 리스트에서 모든 노드 제거
     for(int i = 0; i < count; i++){
         Current = SLL_GetNodeAt(List, i);
